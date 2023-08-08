b2d4ba096de3d085a582ee2e3572a83a95996c3d
bobbyzhong <3bobbyzhong3@gmail.com>
2023-08-07 19:26:11 -0700

    commented out old pages

--- src/app/play/mcq/[checkInId]/page.tsx
+++ src/app/play/mcq/[checkInId]/page.tsx
@@ -1,39 +1,39 @@
-import { getAuthSession } from "@/lib/nextauth";
+// import { getAuthSession } from "@/lib/nextauth";
 
-import React from "react";
-import { redirect } from "next/navigation";
-import { prisma } from "@/lib/db";
-import MCQ from "@/components/MCQ";
+// import React from "react";
+// import { redirect } from "next/navigation";
+// import { prisma } from "@/lib/db";
+// import MCQ from "@/components/MCQ";
 
-type Props = {
-    params: {
-        checkInId: string;
-    };
-};
-const MCQPage = async ({ params: { checkInId } }: Props) => {
-    const session = await getAuthSession();
-    if (!session?.user) {
-        return redirect("/");
-    }
-    const checkIn = await prisma.checkIn.findUnique({
-        where: {
-            id: checkInId,
-        },
-        include: {
-            questions: {
-                select: {
-                    id: true,
-                    question: true,
-                    options: true,
-                },
-            },
-        },
-    });
+// type Props = {
+//     params: {
+//         checkInId: string;
+//     };
+// };
+// const MCQPage = async ({ params: { checkInId } }: Props) => {
+//     const session = await getAuthSession();
+//     if (!session?.user) {
+//         return redirect("/");
+//     }
+//     const checkIn = await prisma.checkIn.findUnique({
+//         where: {
+//             id: checkInId,
+//         },
+//         include: {
+//             questions: {
+//                 select: {
+//                     id: true,
+//                     question: true,
+//                     options: true,
+//                 },
+//             },
+//         },
+//     });
 
-    if (!checkIn || checkIn.checkInType !== "mcq") {
-        return redirect("/quiz");
-    }
+//     if (!checkIn || checkIn.checkInType !== "mcq") {
+//         return redirect("/quiz");
+//     }
 
-    return <MCQ game={checkIn} />;
-};
-export default MCQPage;
+//     return <MCQ game={checkIn} />;
+// };
+// export default MCQPage;
