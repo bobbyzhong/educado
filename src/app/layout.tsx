1c3fc7a151072587412465efd05b22fc56343778
bobbyzhong <3bobbyzhong3@gmail.com>
2023-09-07 11:15:42 -0700

    added google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import Providers from "@/components/Providers";
 import { Toaster } from "@/components/ui/toaster";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,20 +20,37 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
-            <link rel="icon" href="/icons/pearemoji.png" />
-            <body
-                className={cn(
-                    inter.className,
-                    "antialiased min-h-screen pt-16"
-                )}
-            >
-                <Providers>
-                    <Navbar />
-                    {children}
-                    <Toaster />
-                </Providers>
-            </body>
-        </html>
+        <>
+            <Script
+                strategy="lazyOnload"
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            />
+
+            <Script strategy="lazyOnload">
+                {`
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                    page_path: window.location.pathname,
+                    });
+                `}
+            </Script>
+            <html lang="en">
+                <link rel="icon" href="/icons/pearemoji.png" />
+                <body
+                    className={cn(
+                        inter.className,
+                        "antialiased min-h-screen pt-16"
+                    )}
+                >
+                    <Providers>
+                        <Navbar />
+                        {children}
+                        <Toaster />
+                    </Providers>
+                </body>
+            </html>
+        </>
     );
 }
