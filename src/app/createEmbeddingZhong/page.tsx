74100c1e3d4aa5f33697106fcc2339ead663f3c5
bobbyzhong <3bobbyzhong3@gmail.com>
2023-09-21 14:36:36 -0700

    added myleah report

--- src/app/createEmbeddingZhong/page.tsx
+++ src/app/createEmbeddingZhong/page.tsx
@@ -1,4 +1,7 @@
 "use client";
+
+import { Button } from "@/components/ui/button";
+
 type Props = {};
 const CreateEmbedding = (props: Props) => {
     async function createIndexAndEmbeddings() {
@@ -13,11 +16,28 @@ const CreateEmbedding = (props: Props) => {
             console.log("Error: ", err);
         }
     }
+
+    async function calculate() {
+        try {
+            console.log("started");
+            const result = await fetch("/api/calculate", {
+                method: "POST",
+            });
+            const json = await result.json();
+            console.log("Result: ", json);
+        } catch (err) {
+            console.log("Error: ", err);
+        }
+    }
     return (
         <div className="absolute flex flex-col -translate-x-1/2 mt-8 -translate-y-1/2 top-1/2 left-1/2">
-            <button onClick={createIndexAndEmbeddings}>
+            <Button onClick={createIndexAndEmbeddings}>
                 Create Index and Embedding
-            </button>
+            </Button>
+
+            <Button onClick={calculate} className="mt-5">
+                Calculate
+            </Button>
         </div>
     );
 };
