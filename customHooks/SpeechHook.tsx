b1cb5be5d97cec66d71b9f673186760a537828e6
bobbyzhong <3bobbyzhong3@gmail.com>
2023-11-24 14:31:55 -0800

    added audio input

---
+++ customHooks/SpeechHook.tsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+let recognition: any = null;
+
+if (typeof window !== "undefined" && "webkitSpeechRecognition" in window) {
+    recognition = new window.webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.lang = "en-US";
+}
+
+const useSpeechRecognition = () => {
+    const [text, setText] = useState("");
+    const [isListening, setIsListening] = useState(false);
+
+    useEffect(() => {
+        if (!recognition) return;
+
+        recognition.onresult = (event: any) => {
+            setText(event.results[0][0].transcript);
+            recognition.stop();
+            setIsListening(false);
+        };
+    }, []);
+
+    const startListening = () => {
+        setText("");
+        setIsListening(true);
+        recognition.start();
+    };
+
+    const stopListening = () => {
+        setIsListening(false);
+        recognition.stop();
+    };
+
+    return {
+        text,
+        isListening,
+        startListening,
+        stopListening,
+        hasRecognitionSupport: !!recognition,
+    };
+};
+
+export default useSpeechRecognition;
