896a773cabb798f46f2b7c0063cb0f6dba779d7a
bobbyzhong <3bobbyzhong3@gmail.com>
2024-01-29 11:58:07 -0800

    init district

--- utils.ts
+++ utils.ts
@@ -318,7 +318,7 @@ export const updatePinecone = async (
                     pageContent: chunk.pageContent,
                     txtPath: txtPath,
                     // MAKE SURE IT IS SAME AS THE DROPDOWN OPTION
-                    tutorName: "upton_sinclair",
+                    tutorName: "abe_lincoln",
                     standardName: "",
                     // userName: "Bethany Allen",
                     // textbookName:
