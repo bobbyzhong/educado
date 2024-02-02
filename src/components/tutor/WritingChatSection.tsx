a67e7815e2916d755b9a54b7e63fa693ebe51da6
bobbyzhong <3bobbyzhong3@gmail.com>
2024-02-02 10:12:14 -0800

    added multi language support

--- src/components/tutor/WritingChatSection.tsx
+++ src/components/tutor/WritingChatSection.tsx
@@ -1,14 +1,20 @@
 "use client";
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import ReactMarkdown from "react-markdown";
-import { useChat } from "ai/react";
+
 import Image from "next/image";
 import { Textarea } from "@/components/ui/textarea";
 import useAutosizeTextArea from "@/components/tutor/useAutosizeTextarea";
 import Link from "next/link";
-import { Loader2, Mic, SendHorizonal } from "lucide-react";
-import useSpeechRecognition from "../../../customHooks/SpeechHook";
-import OpenAI from "openai";
+import { Globe, Loader2, Mic, SendHorizonal } from "lucide-react";
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
 
 type Props = {
     tutorName: string;
@@ -128,19 +134,46 @@ export default function WritingChatSection({
     const textAreaRef = useRef<HTMLTextAreaElement>(null);
     useAutosizeTextArea(textAreaRef.current, input);
 
-    const {
-        text,
-        isListening,
-        startListening,
-        stopListening,
-        hasRecognitionSupport,
-    } = useSpeechRecognition();
+    const [isRecording, setIsRecording] = useState(false);
+    const [language, setLanguage] = useState("en-US");
+    const [transcript, setTranscript] = useState("");
 
     useEffect(() => {
-        setInput(text); // Set the input value to text
-    }, [text]);
+        const recognition = new window.webkitSpeechRecognition();
+        recognition.continuous = true;
+        recognition.lang = language;
 
-    const delay = (ms: any) => new Promise((res) => setTimeout(res, ms));
+        recognition.onresult = (event) => {
+            for (let i = event.resultIndex; i < event.results.length; i++) {
+                if (event.results[i].isFinal) {
+                    setTranscript(
+                        (transcript: any) =>
+                            transcript + event.results[i][0].transcript
+                    );
+                    recognition.abort();
+                    setIsRecording(false);
+                }
+            }
+        };
+
+        if (isRecording) {
+            recognition.start();
+        } else {
+            recognition.stop();
+        }
+
+        return () => recognition.abort();
+    }, [isRecording, language]);
+
+    const toggleRecording = () => {
+        setIsRecording(!isRecording);
+    };
+    useEffect(() => {
+        setInput(transcript); // Set the input value to text
+    }, [transcript]);
+    useEffect(() => {
+        setTranscript(input); // Set the input value to text
+    }, [input]);
 
     // ----------------
     // Tutor Chat Section
@@ -289,67 +322,216 @@ export default function WritingChatSection({
                     className="stretch mx-2 flex flex-col gap-3 last:mb-2 md:mx-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl -translate-y-6"
                 >
                     <div className="relative flex flex-col h-full flex-1 items-stretch md:flex-col">
-                        <div
-                            className="flex flex-row justify-center items-center pr-5 w-full py-3 flex-grow md:py-4 text-[18px] pl-2 md:pl-4 relative 
-                             bg-white bg-gradient-to-b dark:border-gray-900/50 rounded-md
-                          shadow-[1px_2px_5px_3px_rgba(0,0,0,0.10)] 
+                        <div className="flex flex-row">
+                            <div
+                                className="flex flex-row justify-center items-center pr-5 w-full py-3 flex-grow md:py-4 text-[18px] pl-2 md:pl-4 relative 
+                             bg-white bg-gradient-to-b dark:border-gray-900/50 rounded-lg
+                          shadow-[1px_1px_2px_2px_rgba(0,0,0,0.1)] 
                        "
-                        >
-                            {hasRecognitionSupport ? (
-                                <div>
-                                    {isListening ? (
-                                        <Mic
-                                            className="cursor-pointer mr-2"
-                                            color="#86D20A"
-                                            onClick={() => {
-                                                stopListening();
-                                            }}
-                                        />
-                                    ) : (
-                                        <Mic
-                                            className="cursor-pointer mr-2"
-                                            color="#D3D3D3"
-                                            onClick={() => {
-                                                startListening();
-                                            }}
-                                        />
-                                    )}
-                                </div>
-                            ) : null}
+                            >
+                                <div className="mr-2 cursor-pointer">
+                                    <DropdownMenu>
+                                        <DropdownMenuTrigger asChild>
+                                            <Globe size={23} color="#D3D3D3" />
+                                        </DropdownMenuTrigger>
+                                        <DropdownMenuContent className="w-56">
+                                            <DropdownMenuLabel>
+                                                Audio Language
+                                            </DropdownMenuLabel>
+                                            <DropdownMenuSeparator />
+                                            <DropdownMenuCheckboxItem
+                                                onClick={() =>
+                                                    setLanguage("ar-AE")
+                                                }
+                                                disabled={language === "ar-AE"}
+                                            >
+                                                Arabic
+                                            </DropdownMenuCheckboxItem>
+                                            <DropdownMenuCheckboxItem
+                                                onClick={() =>
+                                                    setLanguage("zh-CN")
+                                                }
+                                                disabled={language === "zh-CN"}
+                                            >
+                                                Chinese
+                                            </DropdownMenuCheckboxItem>
+                                            <DropdownMenuCheckboxItem
+                                                onClick={() =>
+                                                    setLanguage("zh-HK")
+                                                }
+                                                disabled={language === "zh-HK"}
+                                            >
+                                                Chinese (Cantonese)
+                                            </DropdownMenuCheckboxItem>
 
-                            <Textarea
-                                value={input}
-                                tabIndex={0}
-                                ref={textAreaRef}
-                                style={{
-                                    height: "24px",
-                                    maxHeight: "175px",
-                                    overflowY: "hidden",
-                                }}
-                                placeholder="Ask me anything ..."
-                                className="m-0 w-full min-h-0 shadow-none  resize-none  border-0 bg-transparent p-0 pr-7
+                                            <DropdownMenuCheckboxItem
+                                                onClick={() =>
+                                                    setLanguage("cs")
+                                                }
+                                                disabled={language === "cs"}
+                                            >
+                                                Czech
+                                            </DropdownMenuCheckboxItem>
+                                            <DropdownMenuCheckboxItem
+                                                onClick={() =>
+                                                    setLanguage("nl-NL")
+                                                }
+                                                disabled={language === "nl-NL"}
+                                            >
+                                                Dutch
+                                            </DropdownMenuCheckboxItem>
+                                            <DropdownMenuCheckboxItem
+                                                onClick={() =>
+                                                    setLanguage("en-US")
+                                                }
+                                                disabled={language === "en-US"}
+                                            >
+                                                English
+                                            </DropdownMenuCheckboxItem>
+                                            <DropdownMenuCheckboxItem
+                                                onClick={() =>
+                                                    setLanguage("fr-FR")
+                                                }
+                                                disabled={language === "fr-FR"}
+                                            >
+                                                French
+                                            </DropdownMenuCheckboxItem>
+                                            <DropdownMenuCheckboxItem
+                                                onClick={() =>
+                                                    setLanguage("de-DE")
+                                                }
+                                                disabled={language === "de-DE"}
+                                            >
+                                                German
+                                            </DropdownMenuCheckboxItem>
+                                            <DropdownMenuCheckboxItem
+                                                onClick={() =>
+                                                    setLanguage("it-IT")
+                                                }
+                                                disabled={language === "it-IT"}
+                                            >
+                                                Italian
+                                            </DropdownMenuCheckboxItem>
+                                            <DropdownMenuCheckboxItem
+                                                onClick={() =>
+                                                    setLanguage("ja")
+                                                }
+                                                disabled={language === "ja"}
+                                            >
+                                                Japanese
+                                            </DropdownMenuCheckboxItem>
+                                            <DropdownMenuCheckboxItem
+                                                onClick={() =>
+                                                    setLanguage("ko")
+                                                }
+                                                disabled={language === "ko"}
+                                            >
+                                                Korean
+                                            </DropdownMenuCheckboxItem>
+
+                                            <DropdownMenuCheckboxItem
+                                                onClick={() =>
+                                                    setLanguage("pt-PT")
+                                                }
+                                                disabled={language === "pt-PT"}
+                                            >
+                                                Portuguese
+                                            </DropdownMenuCheckboxItem>
+                                            <DropdownMenuCheckboxItem
+                                                onClick={() =>
+                                                    setLanguage("ru")
+                                                }
+                                                disabled={language === "ru"}
+                                            >
+                                                Russian
+                                            </DropdownMenuCheckboxItem>
+
+                                            <DropdownMenuCheckboxItem
+                                                onClick={() =>
+                                                    setLanguage("es-US")
+                                                }
+                                                disabled={language === "es-US"}
+                                            >
+                                                Spanish
+                                            </DropdownMenuCheckboxItem>
+                                            <DropdownMenuCheckboxItem
+                                                onClick={() =>
+                                                    setLanguage("tr")
+                                                }
+                                                disabled={language === "tr"}
+                                            >
+                                                Turkish
+                                            </DropdownMenuCheckboxItem>
+                                        </DropdownMenuContent>
+                                    </DropdownMenu>
+                                </div>
+
+                                <Textarea
+                                    value={input}
+                                    tabIndex={0}
+                                    ref={textAreaRef}
+                                    style={{
+                                        height: "24px",
+                                        maxHeight: "175px",
+                                        overflowY: "hidden",
+                                    }}
+                                    placeholder="Ask me anything ..."
+                                    className="m-0 w-full min-h-0 shadow-none  resize-none  border-0 bg-transparent p-0 pr-7
                                   focus:ring focus:ring-green text-[17px] rounded-none focus-visible:ring-0  pl-2 dark:text-black
                                   md:pl-0"
-                                onChange={handleInputChange}
-                                onKeyDown={handleKeypress}
-                            />
+                                    onChange={handleInputChange}
+                                    onKeyDown={handleKeypress}
+                                />
+                                {true ? (
+                                    <div>
+                                        {isRecording ? (
+                                            <Mic
+                                                className="cursor-pointer"
+                                                color="#86D20A"
+                                                onClick={() => {
+                                                    toggleRecording();
+                                                }}
+                                            />
+                                        ) : (
+                                            <Mic
+                                                className="cursor-pointer "
+                                                color="#D3D3D3"
+                                                onClick={() => {
+                                                    toggleRecording();
+                                                }}
+                                            />
+                                        )}
+                                    </div>
+                                ) : null}
+                            </div>
                             {isLoading ? (
-                                <Loader2
-                                    color="#86D20A"
-                                    className="animate-spin"
-                                    size={26}
-                                />
+                                <div
+                                    className="flex items-center justify-center bg-white rounded-xl ml-2 
+                               shadow-[1px_1px_1px_1px_rgba(0,0,0,0.1)] 
+                                transition ease-in-out    px-4 py-1"
+                                >
+                                    <Loader2
+                                        color="#86D20A"
+                                        className="animate-spin"
+                                        size={26}
+                                    />
+                                </div>
                             ) : (
-                                <SendHorizonal
-                                    className="cursor-pointer"
+                                <div
                                     onClick={onHandleSubmit}
-                                    color={
-                                        input.length === 0
-                                            ? "#D3D3D3"
-                                            : "#86D20A"
-                                    }
-                                    size={26}
-                                />
+                                    className="flex items-center justify-center cursor-pointer bg-white rounded-xl ml-2 
+                                shadow-[1px_2px_1px_3px_rgba(0,0,0,0.10)] hover:shadow-[1px_1px_1px_1px_rgba(0,0,0,0.1)] 
+                                transition ease-in-out    px-4 py-1"
+                                >
+                                    <SendHorizonal
+                                        color={
+                                            input.length === 0
+                                                ? "#D3D3D3"
+                                                : "#86D20A"
+                                        }
+                                        size={26}
+                                    />
+                                </div>
                             )}
                         </div>
                     </div>
