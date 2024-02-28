7a97acfaab755981faa92bbd5d8bd8d3ff3966e4
bobbyzhong <3bobbyzhong3@gmail.com>
2024-02-28 13:40:56 -0800

    fixed bugs

--- src/app/api/adminDashboard/find-question-count/route.ts
+++ src/app/api/adminDashboard/find-question-count/route.ts
@@ -42,15 +42,13 @@ export async function GET(req: Request, res: Response) {
 
         return NextResponse.json({ count }, { status: 200 });
     } catch (error) {
-        if (error instanceof ZodError) {
-            return NextResponse.json(
-                {
-                    message: error.issues,
-                },
-                {
-                    status: 400,
-                }
-            );
-        }
+        return NextResponse.json(
+            {
+                message: error,
+            },
+            {
+                status: 400,
+            }
+        );
     }
 }
