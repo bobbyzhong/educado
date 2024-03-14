9a5f1cd0a20520bfa612f9347dbc9ae76f96578a
bobbyzhong <3bobbyzhong3@gmail.com>
2024-03-13 17:14:00 -0700

    updates

--- src/components/tutor/TeacherPortalCard.tsx
+++ src/components/tutor/TeacherPortalCard.tsx
@@ -210,6 +210,42 @@ const TeacherPortalCard = ({ userId }: Props) => {
             if (data.message === "updated") {
                 router.push("/dashboard-teacher");
             }
+        } else if (code === "PY9K1") {
+            const res = await fetch("/api/updateTeacherStatus", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    userId: userId,
+                    school: "Placentia Yorba Linda Unified School District",
+                    district: true,
+                }),
+            });
+            const data = await res.json();
+            console.log(data);
+            setLoading(false);
+            if (data.message === "updated") {
+                router.push("/dashboard-teacher");
+            }
+        } else if (code === "EQA92") {
+            const res = await fetch("/api/updateTeacherStatus", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    userId: userId,
+                    school: "Equitas Academy",
+                    district: true,
+                }),
+            });
+            const data = await res.json();
+            console.log(data);
+            setLoading(false);
+            if (data.message === "updated") {
+                router.push("/dashboard-teacher");
+            }
         } else {
             setLoading(false);
             toast({
